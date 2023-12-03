0e28e970de63f205b247c1e7c8961081b437071f
fra <francesco.aru25@gmail.com>
2023-12-03 21:53:53 +0100

    small fix

--- day3/part2/main.cpp
+++ day3/part2/main.cpp
@@ -21,7 +21,6 @@ mtx get_matrix(std::ifstream& fd)
 int build_number(std::vector<char> str, size_t n_pos, int& n)
 {
 	int exp=0, count=0;
-    n = 0;
 	while (isdigit(str[n_pos]))
     {
 		n += (str[n_pos] - '0') * pow(10, exp++);
@@ -34,103 +33,92 @@ int build_number(std::vector<char> str, size_t n_pos, int& n)
 	return (count - 1);
 }
 
-int    find_prod(mtx& matrix, size_t i, size_t j)
+int get_top(mtx& matrix, size_t i, size_t j)
 {
-    int n=0, cnt=0;
-    if (((i > 0) and (j > 0) and isdigit(matrix[i - 1][j - 1])) or 
-        ((i > 0) and isdigit(matrix[i - 1][j])) or 
-        ((i > 0) and (j < matrix[i - 1].size() - 1) and isdigit(matrix[i - 1][j + 1])))
+    int cnt=0, n=0;
+
+    if (!isdigit(matrix[i - 1][j]))
     {
-        if (!isdigit(matrix[i - 1][j]))
-        {
-            if ((j > 0) and isdigit(matrix[i - 1][j - 1]))
-            {
-                cnt += build_number(matrix[i - 1], j - 1, n);
-                while (cnt >= 0)
-                    matrix[i - 1][j - cnt--] = '.';
-                return (n);
-            }
-            else
-            {
-                j++;
-                while ((j < matrix[i - 1].size() - 1) and (isdigit(matrix[i - 1][j + 1])))
-                    j++;
-                cnt += build_number(matrix[i - 1], j, n);
-                while (cnt >= 0)
-                    matrix[i - 1][j - cnt--] = '.';
-                return (n);
-            }
-        }
+        if ((j > 0) and isdigit(matrix[i - 1][j - 1]))
+            j--;
         else
         {
+            j++;
             while ((j < matrix[i - 1].size() - 1) and (isdigit(matrix[i - 1][j + 1])))
                 j++;
-            cnt += build_number(matrix[i - 1], j, n);
-            while (cnt >= 0)
-                matrix[i - 1][j - cnt--] = '.';
-            return (n);
         }
     }
-    else if ((j > 0) and isdigit(matrix[i][j - 1]))
+    else
     {
-        j--;
-        cnt += build_number(matrix[i], j, n);
-        while (cnt >= 0)
-            matrix[i][j - cnt--] = '.';
-        return (n);
+        while ((j < matrix[i - 1].size() - 1) and (isdigit(matrix[i - 1][j + 1])))
+            j++;
     }
-    else if ((j < matrix[i - 1].size() - 1) and isdigit(matrix[i][j + 1]))
+    cnt = build_number(matrix[i - 1], j, n);
+    while (cnt >= 0)
+        matrix[i - 1][j - cnt--] = '.';
+    return(n);
+}
+
+int get_mid(mtx& matrix, size_t i, size_t j)
+{
+    int cnt=0, n=0;
+
+    if ((j > 0) and isdigit(matrix[i][j - 1]))
+        j--;
+    else
     {
         j++;
         while ((j < matrix[i].size() - 1) and (isdigit(matrix[i][j + 1])))
             j++;
-        cnt += build_number(matrix[i], j, n);
-        while (cnt >= 0)
-            matrix[i][j - cnt--] = '.';
-        return (n);
     }
-    else if (((i < matrix.size() - 1) and (j > 0) and isdigit(matrix[i + 1][j - 1])) or 
-        ((i < matrix.size() - 1) and isdigit(matrix[i + 1][j])) or 
-        ((i < matrix.size() - 1) and (j < matrix[i + 1].size() - 1) and isdigit(matrix[i + 1][j + 1])))
+    cnt = build_number(matrix[i], j, n);
+    while (cnt >= 0)
+        matrix[i][j - cnt--] = '.';
+    return (n);
+}
+
+int get_bot(mtx& matrix, size_t i, size_t j)
+{
+    int cnt=0, n=0;
+
+    if (!isdigit(matrix[i + 1][j]))
     {
-        if (!isdigit(matrix[i + 1][j]))
-        {
-            if ((j > 0) and isdigit(matrix[i + 1][j - 1]))
-            {
-                cnt += build_number(matrix[i + 1], j - 1, n);
-                while (cnt >= 0)
-                    matrix[i + 1][j - cnt--] = '.';
-                return (n);
-            }
-            else
-            {
-                j++;
-                while ((j < matrix[i + 1].size() - 1) and (isdigit(matrix[i + 1][j + 1])))
-                    j++;
-                cnt += build_number(matrix[i + 1], j, n);
-                while (cnt >= 0)
-                    matrix[i + 1][j - cnt--] = '.';
-                return (n);
-            }
-        }
+        if ((j > 0) and isdigit(matrix[i + 1][j - 1]))
+            j--;
         else
         {
+            j++;
             while ((j < matrix[i + 1].size() - 1) and (isdigit(matrix[i + 1][j + 1])))
                 j++;
-            cnt += build_number(matrix[i + 1], j, n);
-            while (cnt >= 0)
-                matrix[i + 1][j - cnt--] = '.';
-            return (n);
         }
     }
-    return (0);
+    else
+    {
+        while ((j < matrix[i + 1].size() - 1) and (isdigit(matrix[i + 1][j + 1])))
+            j++;
+    }
+    cnt = build_number(matrix[i + 1], j, n);
+    while (cnt >= 0)
+        matrix[i + 1][j - cnt--] = '.';
+    return(n);
+}
+
+int    find_prod(mtx& matrix, size_t i, size_t j)
+{
+    if (((i > 0) and (j > 0) and isdigit(matrix[i - 1][j - 1])) or ((i > 0) and isdigit(matrix[i - 1][j])) or ((i > 0) and (j < matrix[i - 1].size() - 1) and isdigit(matrix[i - 1][j + 1])))
+        return(get_top(matrix, i, j));
+    else if (((j > 0) and isdigit(matrix[i][j - 1])) or ((j < matrix[i - 1].size() - 1) and isdigit(matrix[i][j + 1])))
+        return(get_mid(matrix, i, j));
+    else if (((i < matrix.size() - 1) and (j > 0) and isdigit(matrix[i + 1][j - 1])) or ((i < matrix.size() - 1) and isdigit(matrix[i + 1][j])) or ((i < matrix.size() - 1) and (j < matrix[i + 1].size() - 1) and isdigit(matrix[i + 1][j + 1])))
+        return(get_bot(matrix, i, j));
+    else
+        return (0);
 }
 
 size_t  part2(char *file_name)
 {
     size_t          tot = 0;
     mtx             matrix;
-    // int n1, n2;
     std::ifstream	inFile(file_name);
 
     if (!inFile.is_open())
@@ -141,14 +129,7 @@ size_t  part2(char *file_name)
         for (size_t j=0; j<matrix[i].size(); j++)
         {
             if (matrix[i][j] == '*')
-            {
-                // std::cout << i << " " << j << '\n';
-                // n1 = find_prod(matrix, i, j);
-                // std::cout <<n1<<'\n';
-                // n2 = find_prod(matrix, i, j);
-                // std::cout <<n2<<'\n';
                 tot += find_prod(matrix, i, j) * find_prod(matrix, i, j);
-            }
         }
     }
     return (tot);
